df6c3e76aeeab1d9c24acef0f85bdcb9ce982633
Van Hoang <khanhvan8591@gmail.com>
2021-06-26 14:51:37 +0200

    add campus_courses_complete.db

--- adviser/utils/campus_sql.py
+++ adviser/utils/campus_sql.py
@@ -1,7 +1,10 @@
 import os
+import sys
 import random
 import re
+import pickle
 from typing import List
+from tqdm import tqdm
 
 import sqlite3
 from lxml.etree import Element
@@ -33,20 +36,16 @@ class Course:
         self.lecturers = []  # as list
 
         # dummy slot values, binary values
-        self.ml = None
-        self.engineering = None
-        self.politics = None
-        self.literature = None
-        self.culture = None
+        self.topics = []
+        self.values = []
 
-    def fill_slot_values(self, slot_values: List[str]):
-        self.ml = slot_values[0]
-        self.engineering = slot_values[1]
-        self.politics = slot_values[2]
-        self.literature = slot_values[3]
-        self.culture = slot_values[4]
+    def topics_to_dict(self) -> dict:
+        mapping = {}
+        for topic, value in zip(self.topics, self.values):
+            mapping[topic] = value
+        return mapping
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             'id': self.id,
             'title': self.title,
@@ -55,19 +54,14 @@ class Course:
             'end_date': self.end_date,
             'semester_hours': self.semester_hours,
             'type': self.type,
-            'language': self.language,
+            'language_taught': self.language,
             'ilias_link': self.ilias_link,
             'description': self.description,
             'objective': self.objective,
             'prerequisite': self.prerequisite,
             'extra_info': self.further_info,
             'institution': self.institution,
-            'lecturers': ', '.join(self.lecturers),
-            'machine_learning': self.ml,
-            'engineering': self.engineering,
-            'politics': self.politics,
-            'literature': self.literature,
-            'culture': self.culture
+            'lecturers': ', and '.join(self.lecturers),
         }
 
 
@@ -147,19 +141,22 @@ def get_course_info(xml: Element) -> Course:
                             course.ilias_link = child.xpath(".//courseUrlDtoList/url")[0].text
 
     # now insert dummy slot values into course
-    slots = []
-    for i in range(5):
-        n = random.randint(0, 1)
-        slots.append('true' if n == 1 else 'false')
-    course.fill_slot_values(slots)
+    # values = get_topics_for_course(course)
+    # course.topics = final_topics
+    # course.values = values
     return course
 
 
-def create_table():
-    """
-    Create table in the SQL database
-    :return: None
-    """
+def get_topics_for_course(course: Course, topics: list) -> list:
+    values = []
+    for i in range(len(topics)):
+        n = random.randint(0, 1)
+        values.append('true' if n == 1 else 'false')
+    assert len(topics) == len(values)
+    return values
+
+
+def create_table(topics: list):
     sql = """CREATE TABLE IF NOT EXISTS campus_courses ( id INTEGER PRIMARY KEY,
                                                         title TEXT NOT NULL,
                                                         lecturers TEXT,
@@ -168,25 +165,33 @@ def create_table():
                                                         end_date TEXT,
                                                         semester_hours TEXT,
                                                         type TEXT,
-                                                        language TEXT,
+                                                        language_taught TEXT,
                                                         ilias_link TEXT,
                                                         description TEXT,
                                                         objective TEXT,
                                                         prerequisite TEXT,
                                                         institution TEXT,
-                                                        extra_info TEXT,
-                                                        machine_learning TEXT,
-                                                        engineering TEXT,
-                                                        politics TEXT,
-                                                        literature TEXT,
-                                                        culture TEXT
+                                                        extra_info TEXT
                                                         )"""
 
     cursor.execute(sql)
     connection.commit()  # commit changes
 
+    # add topic columns into table
+    topic_count = 0
+    for topic in topics:
+        try:
+            cursor.execute('''ALTER TABLE campus_courses ADD COLUMN ''' + topic.lower() + ''' TEXT''')
+            connection.commit()  # commit changes
+            topic_count += 1
+        except sqlite3.OperationalError as e:
+            print('Topic ERROR:', topic.lower())
+            print("count", topic_count)
+            print(e)
+            pass
 
-def insert_data_to_db(course: Course):
+
+def insert_course_to_db(course: Course) -> int:
     columns = []  # list of column names with values to insert into the table
     info = ()  # a set of corresponding values of the fields
 
@@ -202,39 +207,89 @@ def insert_data_to_db(course: Course):
     sql = 'INSERT OR IGNORE INTO campus_courses (' + ','.join(columns) + ') VALUES (' + ','.join(data) + ')'
     cursor.execute(sql, info)  # finally add values into the table
     connection.commit()  # commit the changes
+    return cursor.lastrowid
 
 
-def remove_stopwords(title: str) -> list:
+def insert_topics_to_course(course, topics, course_id):
+    values = get_topics_for_course(course, topics)
+    course.topics = final_topics
+    course.values = values
+
+    mapping = course.topics_to_dict()
+    for topic, value in mapping.items():
+        sql = 'UPDATE campus_courses SET ' + topic + ' = ? WHERE id = ?'
+        cursor.execute(sql, (value, course_id))
+        connection.commit()
+
+
+def insert_data_to_db(courses: List[Course], topics):
+    for course in tqdm(courses):
+        course_id = insert_course_to_db(course)
+        insert_topics_to_course(course, topics, course_id)
+
+
+def remove_stopwords(title: str, en_sw, de_sw) -> list:
     title = re.sub("[!#$%&\\'()*+,-./:;<=>?@^_`{|}~]", '', title)
     title = [word for word in title.strip().split() if word.lower() not in en_sw]
     return [word for word in title if word.lower() not in de_sw]
 
 
+def remove_numbers(text) -> str:
+    return re.sub('\\d+', '', text).strip()
+
+
+def remove_non_words(text) -> str:
+    return re.sub(u'^\\W+|\\W+$', '', text).strip()
+
+
+def clean_ngrams(ngram: str, en_sw, de_sw) -> list:
+    ngram = remove_numbers(ngram)
+    ngram = remove_non_words(ngram)
+    return remove_stopwords(ngram, en_sw, de_sw)
+
+
 def get_ngrams(title: list) -> Counter:
     count = Counter()
     count.update(title)   # add unigrams
-    count.update([' '.join(title[i:i + 2]) for i in range(0, len(title) - 1)])  # add bigrams
-    count.update([' '.join(title[i:i + 3]) for i in range(0, len(title) - 2)])  # add bigrams
+    count.update(['_'.join(title[i:i + 2]) for i in range(0, len(title) - 1)])  # add bigrams
+    count.update(['_'.join(title[i:i + 3]) for i in range(0, len(title) - 2)])  # add bigrams
     return count
 
 
-def write_topics():
+def write_topics(topics: Counter):
     path_to_file = '../resources/databases/topics.txt'
     out = [topic + "\t" + str(count) for topic, count in topics.most_common()]
     with open(path_to_file, 'w', encoding='utf-8') as file:
         file.write('\n'.join(out))
 
 
-if __name__ == "__main__":
+def save_courses(courses: List[Course]):
+    with open(path_to_courses_file, 'wb') as file:
+        pickle.dump(courses, file)
 
-    # create an SQL database
-    # db_path = '../resources/databases/campus_courses.db'
-    db_path = '../resources/databases/dummy_courses.db'
-    connection = sqlite3.connect(os.path.join(os.getcwd(), db_path))  # establish a connection
-    cursor = connection.cursor()  # create the cursor object
-    create_table()
 
+def load_courses() -> List[Course]:
+    print('Loading courses .....')
+    with open(path_to_courses_file, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+
+def load_topics() -> list:
+    topics = []
+    sql_keys = ['foreign']
+    with open(path_to_topic_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            if len(line.strip().split('\t')) == 2:
+                ngrams, count = line.strip().split('\t')
+                if int(count) >= 10 and len(ngrams) > 3 and ngrams.lower() not in sql_keys:
+                    topics.append(ngrams.strip().lower())
+    return list(set(topics))  # remove duplicates
+
+
+def download_courses_and_topics():
     # retrieve courses from the api in batch
+    print('Access to api for downloading courses ....')
     api = CampusApi(30)
     batch = api.get_next_batch_of_courses()
 
@@ -251,12 +306,34 @@ if __name__ == "__main__":
         for course_xml in batch:
             # turn course xml into Course object and insert into the sql db
             course = get_course_info(xml=course_xml)
-            topics.update(get_ngrams(remove_stopwords(course.title)))  # from title
-            topics.update(get_ngrams(remove_stopwords(course.institution)))  # from institution
+            topics.update(get_ngrams(clean_ngrams(course.title, en_sw, de_sw)))  # from title
+            topics.update(get_ngrams(clean_ngrams(course.institution, en_sw, de_sw)))  # from institution
+            courses.append(course)
 
-            # courses.append(course)
             # insert_data_to_db(get_course_info(xml=course_xml))
         batch = api.get_next_batch_of_courses()
 
-    write_topics()
+    write_topics(topics)
+    save_courses(courses)
     print('Done with {} courses'.format(courses_count))
+    return courses
+
+
+if __name__ == "__main__":
+
+    # db_path = '../resources/databases/campus_courses.db'
+    db_path = '../resources/databases/campus_courses_complete.db'
+    path_to_courses_file = '../resources/databases/courses.pk'
+    path_to_topic_file = '../resources/databases/topics.txt'
+
+    all_courses = load_courses() if os.path.exists(path_to_courses_file) else download_courses_and_topics()
+    final_topics = load_topics()  # get topics
+    print("Total topics: ", len(final_topics))
+
+    # SQD database management
+    connection = sqlite3.connect(os.path.join(os.getcwd(), db_path))  # establish a connection
+    cursor = connection.cursor()  # create the cursor object
+
+    print('Create and insert data into table...')
+    create_table(final_topics)
+    insert_data_to_db(all_courses, final_topics)
